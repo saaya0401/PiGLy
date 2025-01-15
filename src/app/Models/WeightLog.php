e301fb9e9aaa0853354fbcfb71269234a82f93d6
koba saaya <saaya.koba0401@gmail.com>
2025-01-15 16:09:05 +0900

    adminviewデータ表示

--- src/app/Models/WeightLog.php
+++ src/app/Models/WeightLog.php
@@ -4,12 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class WeightLog extends Model
 {
     use HasFactory;
+
     protected $fillable=['user_id', 'date', 'weight', 'calories', 'exercise_time', 'exercise_content'];
+
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function getDateAttribute($value){
+        if(!$value){
+            return null;
+        }
+        return Carbon::createFromFormat('Y-m-d', $value)->format('Y/m/d');
+    }
+
+    public function getExerciseTimeAttribute($value){
+        if(!$value){
+            return null;
+        }
+        return Carbon::createFromFormat('H:i:s', $value)->format('H:i');
     }
 }
--- src/database/migrations/2025_01_13_164235_create_weight_target_table.php
+++ src/database/migrations/2025_01_13_164235_create_weight_target_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWeightTargetsTable extends Migration
+class CreateWeightTargetTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateWeightTargetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('weight_targets', function (Blueprint $table) {
+        Schema::create('weight_target', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->decimal('target_weight', 4, 1);
@@ -28,6 +28,6 @@ class CreateWeightTargetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('weight_targets');
+        Schema::dropIfExists('weight_target');
     }
 }
